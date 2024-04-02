e322c1c81d358da72774cef5ee06ec5d653124cb
Tarllysson Silva <tharllysonsilva@gmail.com>
2024-04-02 16:24:42 -0300

    Estrutura de dados

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:testing639/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  late final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    //obtendo a permissão do usuario
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    //buscar localização atual
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    //Converter o local em uma lista de objetos de marcador
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    //Extrai o nome da cidade do primeiro marcador
+    String? city = placemark[0].locality;
+
+    return city ?? "";
+  }
+}
