d0dddd7bc364505b3ce35bcb943829176436c3b4
Tarllysson Silva <tharllysonsilva@gmail.com>
2024-04-03 16:54:06 -0300

    Tela de login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:testing639/components/my_botton.dart';
+import 'package:testing639/components/my_textfield.dart';
+import 'package:testing639/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 100, 100, 20),
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('assets/icon.png'),
+                ),
+              ),
+              Text(
+                'Seja bem-vindo ao Weather',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+
+              //user textfield
+
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Nome do usuario',
+                obscureText: false,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              //password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Senha',
+                obscureText: true,
+              ),
+
+              //forget password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
+              ),
+              MyButton(
+                onTap: signUserIn,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Ou continuar com',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'assets/google.png'),
+                  SizedBox(width: 25),
+                  SquareTile(imagePath: 'assets/facebook.png'),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Não é membro?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Resgistre agora',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
