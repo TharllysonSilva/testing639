6fcab987a9a1756dbf4b26c82e5d0f032727e739
Tarllysson Silva <tharllysonsilva@gmail.com>
2024-04-03 18:44:35 -0300

    integração com firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:testing639/pages/login_page.dart';
 import 'package:testing639/pages/weather_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:testing639/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
